68f4f201c6a8dcdd54fce6b51cd3fe6d16a8968c
griffio <griffio@users.noreply.github.com>
2018-09-02 22:24:35 +0100

    csrf tokens form post
    Add templates for csrf
    index.ejs

--- server.js
+++ server.js
@@ -4,27 +4,36 @@ const multer  = require("multer");
 const webpack = require("webpack");
 const webpackMiddleware = require("webpack-dev-middleware");
 const webpackConfig = require("./webpack.config");
-const publicPath = path.join(__dirname, "public");
+const staticPath = path.join(__dirname, "static");
 const port = process.env.PORT || 9000;
 const app = express();
-const router = express.Router();
 const upload = multer();
 const helmet = require("helmet");
 const ejs = require("ejs");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const csrfProtection = csrf({ cookie: true });
 
+app.use(express.static(staticPath));
+app.use(cookieParser());
 app.use(helmet()); // basic web security headers
-app.use(express.static(publicPath));
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+app.set("view engine", "ejs");
+
+app.get("/", csrfProtection, (req, res) => {
+  res.render("index", { csrfToken: req.csrfToken() });
 });
 
-router.post("/greet", upload.none(), (req, res) => {
+// csrfProtection position requires formdata as it looks for body._csrf
+app.post("/greet", upload.none(), csrfProtection, (req, res) => {
   if (!req.body) return res.sendStatus(400);
   const greeting = req.body["greeting"];
-  result = ejs.render("And <%= greeting %> to you!", {greeting: greeting});
+  const result = ejs.render("And <%= greeting %> to you!", {greeting: greeting});
   res.send(result);
 });
 
-app.use('/', router);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
+
